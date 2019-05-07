7d5f8a936973e2f5f22a103806bd85971e56a248
Dev <dbrhy95@gmail.com>
2019-05-06 23:06:51 -0500

    make weather prettier

--- modules/weather/weather.py
+++ modules/weather/weather.py
@@ -17,10 +17,10 @@ def getWeather(input, weatherKey):
         forecast = weatherClient.forecast(q=input, days=2)
         current = weatherClient.current(q=input)
 
-        reply = str(current['location']['name']) + ": currently " + str(current['current']['temp_f']) + "°F/"+ str(current['current']['temp_c']) + "°C and " + str(current['current']['condition']['text']).lower() 
+        reply = "**" + str(current['location']['name']) + "** " + convertWeatherToEmoji(current['current']['condition']['text']) + " currently " + str(current['current']['temp_f']) + "°F/"+ str(current['current']['temp_c']) + "°C and " + str(current['current']['condition']['text']).lower() 
         reply += "\nCurrent wind speed: " + str(current['current']['wind_mph']) + " mph/" + str(current['current']['wind_kph']) + " kph"
         for day in forecast['forecast']['forecastday']:
-            reply += "\n\n• " + str(day['date']) + ": " + str(day['day']['condition']['text']).lower()
+            reply += "\n\n" + convertWeatherToEmoji(str(day['day']['condition']['text'])) + " " + str(day['date']) + ": " + str(day['day']['condition']['text']).lower()
             reply += "\nAvg. Temperature: " + str(day['day']['avgtemp_f']) + "°F/" + str(day['day']['avgtemp_c']) + "°C"
 
     except Exception as e: 
@@ -28,3 +28,22 @@ def getWeather(input, weatherKey):
         print(e)
         
     return reply
+
+def convertWeatherToEmoji(condition):
+    condition = condition.lower()
+    emoji = ''
+    if 'torrential rain shower' in condition:
+        emoji = '🌊'
+    elif 'thunder' in condition:
+        emoji = '🌩'
+    elif 'rain' in condition:
+        emoji = '🌧'
+    elif 'fog' in condition:
+        emoji = '🌫'
+    elif 'partly cloudy' in condition:
+        emoji = '⛅'
+    elif 'cloudy' in condition:
+        emoji = '☁'
+    elif 'sunny' in condition or 'clear' in condition:
+        emoji = '☀'
+    return emoji
