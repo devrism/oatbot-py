062b0d94128183725a0071d05b184906c9fbad16
Dev <dbrhy95@gmail.com>
2020-08-05 20:05:00 -0500

    holy moly they broke it all

--- oatbot.py
+++ oatbot.py
@@ -6,16 +6,15 @@ oatbot created by devrism, exclusively for the friend chat junk land mark II Dis
 """
 
 import discord
-from res.config import *
+from res.configtest import *
 from modules.thonkify.thonkify import thonkify
 from modules.weather.weather import getWeather
 from modules.activityGroup.activityGroup import *
 
 client = discord.Client()
-
+playingStatus = discord.Game('socially safe games') #name of the game the bot plays
 PREFIX = 'oat' #TODO: add function to change prefix later
 separator = '/'
-playing = 'socially safe games' #name of the game the bot plays
 
 #Initialize ActivityManager
 activityManager = ActivityGroupManager()
@@ -27,12 +26,12 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name=playing))
+    await client.change_presence(status=discord.Status.online, activity=playingStatus)
 
 @client.event
 async def on_resumed():
     # change game status
-    await client.change_presence(game=discord.Game(name=playing))
+    await client.change_presence(status=discord.Status.online, activity=playingStatus)
     
 @client.event
 async def on_message(message):
@@ -43,45 +42,45 @@ async def on_message(message):
         ############################################## slash commands ################################################
         if lower_msg.startswith('/weather '): #weather
             reply = getWeather(message, weatherKey)
-            await client.send_message(message.channel, "This is under construction, sorry!")
+            await message.channel.send("This is under construction, sorry!")
 
         if lower_msg.startswith('/clap '): 
             reply = message.content[5:].replace(" ", " 👏 ") + " 👏"
-            await client.send_message(message.channel, reply)
+            await message.channel.send(reply)
             
         #thonkify 
         elif lower_msg.startswith('/thonkify '):
             if len(message.content) > 60:
-                await client.send_message(message.channel, 'Error: message cannot be over 60 characters')
+                await message.channel.send('Error: message cannot be over 60 characters')
             else:
                 thonkify(message)
-                await client.send_file(message.channel, 'result.png')
+                await message.channel.send(file=discord.File('result.png'))
 
         ############################################## reactions #####################################################
 
         #responds to oats with bandaid emoji
         elif '/oat' in lower_msg:
-            await client.add_reaction(message, 'name:304863358325358602')
+            await message.add_reaction('name:304863358325358602')
             # await client.add_reaction(message, 'name:457245675072258078')
             
         #bacAgreed
         elif 'naruhodo' in lower_msg:
-            emoji = discord.utils.get(client.get_all_emojis(), id='555426896805101586')
-            await client.add_reaction(message, emoji)
+            emoji = client.get_emoji(555426896805101586)
+            await message.add_reaction(emoji)
 
         #Dr. Pimplepopper is gross af
         elif 'pimplepopper' in lower_msg:
-            await client.add_reaction(message, '\U0001F922')
+            await message.add_reaction('\U0001F922')
 
         #put that table back where it came from, or so help me
         elif '(╯°□°）╯︵ ┻━┻' in lower_msg:
-            await client.send_message(message.channel, '┬──┬ ノ( ゜-゜ノ);;')
+            await message.channel.send('┬──┬ ノ( ゜-゜ノ);;')
 
         elif 'yeet' in lower_msg:
-            await client.send_message(message.channel, 'yeet!')
+            await message.channel.send('yeet!')
 
         elif 'good morning' in lower_msg: 
-            await client.add_reaction(message, '\U0001F31E')
+            await message.add_reaction('\U0001F31E')
 
         #if the message starts with our designated prefix, process the command
         #if message.content.startswith(PREFIX+separator):
